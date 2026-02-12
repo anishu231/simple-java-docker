4156ddff54c48e54187de5aa0b0cff397ef7ad91
anish-9857 <124656367+anishu231@users.noreply.github.com>
2026-02-13 01:20:27 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
-
-FROM eclipse-temurin:17-jdk-alpine
-
-
-
-LABEL maintainer="your-email@example.com"
-LABEL version="1.0"
-LABEL description="A simple Java application"
-
-
-WORKDIR /app
-
-
-COPY src/Main.java /app/Main.java
-
-
-RUN javac Main.java
-
-CMD ["java", "Main"]
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Docker Java App!");
+    }
+}
